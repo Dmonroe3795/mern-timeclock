308ed7bd2dcb323e2b49093516766ef7bd02668d
Wendel Bishop <Wendelbishop@gmail.com>
2019-04-13 13:20:50 -0400

    Changed routing to allow for member and admin dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -6,37 +6,158 @@ import MemberGrid from './components/MemberGrid';
 import { BrowserRouter, Link, Route } from 'react-router-dom'
 import './App.css';
 
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import SwipeableViews from 'react-swipeable-views';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
 WebFont.load({
   google: {
     families: ['Roboto']
   }
 });
 
+function TabContainer({ children, dir }) {
+  return (
+    <Typography component="div" dir={dir} style={{ padding: 8 * 3 }}>
+      {children}
+    </Typography>
+  );
+}
+
+const styles = theme => ({
+  root: {
+    backgroundColor: theme.palette.background.paper,
+    width: 500,
+    spacing: 10
+  }
+});
+
+TabContainer.propTypes = {
+  children: PropTypes.node.isRequired,
+  dir: PropTypes.string.isRequired,
+};
+
+
 class App extends Component {
+  state = {
+    value: 0,
+  };
+
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
+  handleChangeIndex = index => {
+    this.setState({ value: index });
+  };
+
+
+
   render() {
+    const { classes, theme } = this.props;
+
     return (
       <BrowserRouter>
-        <div className='App'>
-          <NavBar />
-          <Grid container spacing={24} style={{padding:24}}>
-            <Grid item xs={12} sm= {6} lg={4} xl={3}>
+      <NavBar/>
+        <div className={classes.root}>
+      </div>
+
+      <div className='App'>
+          <Grid container direction='column' alignContent='center' spacing={24} style={{padding:24}}>
               <Link to="/">
               Home
               </Link>
-            </Grid>
-            <Grid item xs={12} sm= {6} lg={4} xl={3}>
-              <Link to="/members">
-              Members
-              </Link>
-            </Grid>
           </Grid>
 
-          <Route exact path="/" render={() => <h1>Welcome to the Home Page. Click Members link</h1>}/>
-          <Route path="/members" component={MemberGrid}/>
+          <Route exact path="/" render={() => 
+            <Grid container direction="column" alignItems="center">
+            <AppBar position="static" color="default" style={{width: "50vw"}}>
+                <Tabs
+                  value={this.state.value}
+                  onChange={this.handleChange}
+                  indicatorColor="primary"
+                  textColor="primary"
+                  variant="fullWidth"
+                >
+                  <Tab label="Member" />
+                  <Tab label="Admin" />
+                </Tabs>
+          </AppBar>
+          <SwipeableViews className="MuiPaper-elevation4-17 full-width" style={{width: "50vw"}}
+            axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+            index={this.state.value}
+            onChangeIndex={this.handleChangeIndex}
+          >
+            <TabContainer dir={theme.direction}>
+            <Grid container direction="column" alignItems="center">
+              <TextField style={{width: "50%"}}
+                id="standard-with-placeholder"
+                label="Email"
+                placeholder="Enter Email"
+                className={classes.textField}
+                margin="normal"
+              />
+              <TextField style={{width: "50%"}}
+                id="standard-with-placeholder"
+                label="Password"
+                placeholder="Enter Password"
+                className={classes.textField}
+                margin="normal"
+                type="password"
+                autoComplete="current-password"
+              />
+              <Link exact to="/Dashboard" style={{textDecoration: 'none', width: '50%'}} >
+              <Button  variant="outlined" color="primary"  size="large" style={{width: "100%"}} className={classes.margin}>
+                Login
+                </Button>
+              </Link>
+            </Grid>
+            </TabContainer>
+            <TabContainer dir={theme.direction}>
+            <Grid container direction="column" alignItems="center">
+              <TextField style={{width: "50%"}}
+                id="standard-with-placeholder"
+                label="Email"
+                placeholder="Enter Email"
+                className={classes.textField}
+                margin="normal"
+              />
+              <TextField style={{width: "50%"}}
+                id="standard-with-placeholder"
+                label="Password"
+                placeholder="Enter Password"
+                className={classes.textField}
+                margin="normal"
+                type="password"
+                autoComplete="current-password"
+              />
+              <Link exact to="/AdminDashboard" style={{textDecoration: 'none', width: '50%'}} >
+              <Button  variant="outlined" color="primary"  size="large" style={{width: "100%"}} className={classes.margin}>
+                Login
+                </Button>
+              </Link>
+            </Grid>
+            </TabContainer>
+          </SwipeableViews>
+          </Grid>
+          }/>
+          <Route path="/AdminDashboard" component={MemberGrid}/>
+          <Route path="/Dashboard" component={MemberGrid}/>
         </div>
       </BrowserRouter>
     );
   }
 }
 
-export default App;
+App.propTypes = {
+  classes: PropTypes.object.isRequired,
+  theme: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles, { withTheme: true })(App);
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,10 +2,13 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
+import { BrowserRouter, Link, Route } from 'react-router-dom'
+import Login from './Login';
 
 const NavBar = () => {
     return(
-        <div>
+        <BrowserRouter>
+        <div style={{ marginBottom: 30}}>
             <AppBar position="static">
                 <Toolbar>
                     <Typography variant="title" color="inherit">
@@ -15,6 +18,7 @@ const NavBar = () => {
 
             </AppBar>
         </div>
+        </BrowserRouter>
     )
 }
 
