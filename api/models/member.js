ee0a71f7f7db0b83cfcfb794be34ced4e274b889
Damion <monroedc@battlers.ab.edu>
2019-04-12 22:08:51 -0400

    fixed member duplication

--- api/models/member.js
+++ api/models/member.js
@@ -1,14 +1,5 @@
 const mongoose = require('mongoose');
 
-const memberSchema = mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
-    name: String,
-    totalHours: Number
-});
-
-module.exports = mongoose.model('Member', memberSchema);
-
-//const mongoose = require('mongoose');
 const SessionSchema = require('session');
 
 const memberSchema = mongoose.Schema({
