889149e0e1b0a8680dc7f325c1f9fc18c1657fbf
Wendel Bishop <Wendelbishop@gmail.com>
2019-04-19 17:32:21 -0400

    Changed recent activity card

--- client/src/components/RecentActivity/RecentSessionCard.js
+++ client/src/components/RecentActivity/RecentSessionCard.js
@@ -7,34 +7,19 @@ import ListItemIcon from '@material-ui/core/ListItemIcon'
 import PartnerIcon from '@material-ui/icons/Business';
 import UserIcon from '@material-ui/icons/Person'
 import DurationIcon from '@material-ui/icons/Timer'
+import GroupIcon from '@material-ui/icons/Group'
 
 export default class RecentSessionCard extends React.Component {
-
-    // state = {
-    //     member: ""
-    // }
 
     constructor(props) {
         super(props)
         this.state = {
             member: props.session.member.name,
-            partner: props.session.partner.organization,
+            partner: props.session.partner.name,
+            group: props.session.group.name,
             duration: props.session.duration
         }
     }
-
-    // loadData() {
-    //     fetch(`/members/${this.props.session.member}`)
-    //         .then(response => response.json())
-    //         .then(data => {
-    //             this.setState({ member: data.name });
-    //         })
-    //         .catch(err => console.error(this.props.url, err.toString()))
-    // }
-
-    // componentDidMount() {
-    //     this.loadData()
-    // }
 
     render() {
         const { duration } = this.props.session;
@@ -45,7 +30,7 @@ export default class RecentSessionCard extends React.Component {
                         <ListItemIcon>
                             <UserIcon />
                         </ListItemIcon>
-                        <ListItemText primary={`${this.state.member}`} />
+                        <ListItemText primary={this.state.member} />
                         <ListItemText align="right" inset primary={`${duration.toFixed(2)} hrs`} />
                         <ListItemIcon>
                             <DurationIcon />
@@ -55,7 +40,11 @@ export default class RecentSessionCard extends React.Component {
                         <ListItemIcon>
                             <PartnerIcon />
                         </ListItemIcon>
-                        <ListItemText primary={`${this.state.partner}`} />
+                        <ListItemText primary={this.state.partner} />
+                        <ListItemText align="right" inset primary={this.state.group} />
+                        <ListItemIcon>
+                            <GroupIcon />
+                        </ListItemIcon>
                     </ListItem>
                 </List>
             </Paper>
